3e10a5777d53224245c2bf00cc5f4209b6bd26eb
andresam <andres@aamsis.co>
2023-10-14 23:24:54 -0500

    add testing lines for parsers

--- src/invoice_spec.clj
+++ src/invoice_spec.clj
@@ -1,6 +1,8 @@
 (ns invoice-spec
   (:require
-    [clojure.spec.alpha :as s]))
+    [clojure.spec.alpha :as s])
+  (:require [invoice-item :refer :all])
+  )
 
 (defn not-blank? [value] (-> value clojure.string/blank? not))
 (defn non-empty-string? [x] (and (string? x) (not-blank? x)))
@@ -30,6 +32,11 @@
 (s/def :invoice/items (s/coll-of ::invoice-item :kind vector? :min-count 1))
 
 (s/def ::invoice
-  (s/keys :req [:invoice/issue-date
+  (s/keys :req [
                 :invoice/customer
-                :invoice/items]))
+                ]))
+
+; (println (s/valid? ::invoice { :invoice/customer { :customer/name "ANDRADE RODRIGUEZ MANUEL ALEJANDRO", :customer/email "cgallegoaecu@gmail.com" } } ))
+
+(println jsonInvoice)
+(println (str "is valid: " (s/valid? ::invoice jsonInvoice)))
